7a8249beb8d7db2453c2d40491887ecd6d74ec04
Picatchu <72950145+BizcuitMild@users.noreply.github.com>
2022-08-12 06:52:55 +0700

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'adamant'
+games { 'gta5' }
+
+author '𝐼𝑡𝑠𝑚𝑒𝑚𝑖𝑙𝑑.#7372'
+name 'GitHub Updater'
+description 'GitHub Updater Releases Assets By Samild.'
+version '1.0'
+
+shared_scripts {
+	'config.lua'
+}
+
+client_scripts {
+	'client.lua'
+}
+
+server_scripts {
+	'server.lua'
+}
